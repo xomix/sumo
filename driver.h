32e23555282cbc74bae61081e499a9dab318da8d
Jaume Aguilera Badia <txomix@gmail.com>
2018-03-15 21:41:37 -0300

    Corrected driver.h function names

--- driver.h
+++ driver.h
@@ -1,13 +1,13 @@
 #ifndef DRIVER_H_ /* DRIVER_H_ header guard */
 #define DRIVER_H_
 
-void motor_init(void);
-void motor_move_motor1(int8_t speed);
-void motor_move_motor2(int8_t speed);
+void driver_init(void);
+void driver_move_motor1(int8_t speed);
+void driver_move_motor2(int8_t speed);
 /* move both motors:
  *	speed sign means forward or backward
  *	speed value means dutty cycle (speed)
  */
-void motor_move(int8_t speed1, int8_t speed2);
+void driver_move(int8_t speed1, int8_t speed2);
 
 #endif /* DRIVER_H_ header guard */
