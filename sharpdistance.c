e88b8b44b8fcb73474dc5b58321a47a225c307e7
rom973 <romain.mouchet@gmail.com>
2018-03-18 00:02:14 -0300

    ajout de filtre distance IR

--- sharpdistance.c
+++ sharpdistance.c
@@ -1,5 +1,11 @@
 #include "analog.h"
 
+/* Seuil de voltage pour declencher une
+ détection de l'IR
+ */
+#define DETECTION 1U
+/* voltage 1 = distance sup à 50 cm*/
+
 void sharp_init(void)
 {
 	/* initialise analog registers */
@@ -18,5 +24,10 @@ uint8_t sharp_add_sensor(uint8_t pin)
 uint16_t sharp_distance(uint8_t pin)
 {
 	/* TODO (Jaume): fit polynomical voltage to distance function*/
-	return analog_voltage(pin);
+    uint16_t detection = 0;
+    uint16_t voltage = analog_voltage(pin);
+    if (voltage < DETECTION) {
+        detection = 1;
+    }
+	return detection;
 }
