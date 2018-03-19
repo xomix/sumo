0651d0e533f5ca322b0e710cffb25bd434548333
Jaume Aguilera Badia <txomix@gmail.com>
2018-03-19 02:47:58 -0300

    Corrects bug when comparing to minimum voltage

--- sharpdistance.c
+++ sharpdistance.c
@@ -3,8 +3,8 @@
 /* Seuil de voltage pour declencher une
  détection de l'IR
  */
-#define DETECTION 1U
-/* voltage 1 = distance sup à 50 cm*/
+#define DETECTION 1
+/* voltage < 1V => distance sup à 50 cm*/
 
 void sharp_init(void)
 {
@@ -26,7 +26,7 @@ uint16_t sharp_distance(uint8_t pin)
 	/* TODO (Jaume): fit polynomical voltage to distance function*/
     uint16_t detection = 0;
     uint16_t voltage = analog_voltage(pin);
-    if (voltage < DETECTION) {
+    if (voltage > DETECTION) {
         detection = 1;
     }
 	return detection;
